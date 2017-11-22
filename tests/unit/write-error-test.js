048d1646da764c9fbe0fcfcb6387fff7b3dcd67b
Josemar Luedke <josemarluedke@gmail.com>
2017-11-21 22:49:14 -0800

    Extract useful info from errors on writeError & clean up

--- tests/unit/write-error-test.js
+++ tests/unit/write-error-test.js
@@ -27,16 +27,17 @@ describe('writeError', function() {
     }));
 
     expect(ui.output).to.equal('');
-    expect(ui.errors).to.equal(chalk.red('build error') + EOL);
+    expect(ui.errors).to.equal(chalk.red('build error') + EOL + EOL);
   });
 
   it('error with stack', function() {
+    ui.setWriteLevel('DEBUG')
     writeError(ui, new BuildError({
       stack: 'the stack'
     }));
 
-    expect(ui.output).to.equal('');
-    expect(ui.errors).to.equal(chalk.red('Error') + EOL + 'the stack' + EOL);
+    expect(ui.output).to.equal(chalk.red('the stack') + EOL);
+    ui.setWriteLevel('INFO');
   });
 
   it('error with file', function() {
@@ -45,7 +46,7 @@ describe('writeError', function() {
     }));
 
     expect(ui.output).to.equal('');
-    expect(ui.errors).to.equal(chalk.red('File: the file') + EOL + chalk.red('Error') + EOL);
+    expect(ui.errors).to.equal(chalk.red('File: the file') + EOL + EOL + chalk.red('Error') + EOL + EOL);
   });
 
   it('error with filename (as from Uglify)', function() {
@@ -54,7 +55,7 @@ describe('writeError', function() {
     }));
 
     expect(ui.output).to.equal('');
-    expect(ui.errors).to.equal(chalk.red('File: the file') + EOL + chalk.red('Error') + EOL);
+    expect(ui.errors).to.equal(chalk.red('File: the file') + EOL + EOL + chalk.red('Error') + EOL + EOL);
   });
 
   it('error with file + line', function() {
@@ -64,7 +65,7 @@ describe('writeError', function() {
     }));
 
     expect(ui.output).to.equal('');
-    expect(ui.errors).to.equal(chalk.red('File: the file (the line)') + EOL + chalk.red('Error') + EOL);
+    expect(ui.errors).to.equal(chalk.red('File: the file:the line') + EOL + EOL + chalk.red('Error') + EOL + EOL);
   });
 
   it('error with file + col', function() {
@@ -74,7 +75,7 @@ describe('writeError', function() {
     }));
 
     expect(ui.output).to.equal('');
-    expect(ui.errors).to.equal(chalk.red('File: the file') + EOL + chalk.red('Error') + EOL);
+    expect(ui.errors).to.equal(chalk.red('File: the file') + EOL + EOL + chalk.red('Error') + EOL + EOL);
   });
 
   it('error with file + line + col', function() {
@@ -85,6 +86,105 @@ describe('writeError', function() {
     }));
 
     expect(ui.output).to.equal('');
-    expect(ui.errors).to.equal(chalk.red('File: the file (the line:the col)') + EOL + chalk.red('Error') + EOL);
+    expect(ui.errors).to.equal(chalk.red('File: the file:the line:the col') + EOL + EOL + chalk.red('Error') + EOL + EOL);
+  });
+
+  it('error title: file + line + column + errorType + nodeName', function() {
+    writeError(ui, new BuildError({
+      file: 'index.js',
+      line: '10',
+      col:  '15',
+      broccoliPayload: {
+        broccoliNode: {
+          nodeName: 'Babel'
+        },
+        error: {
+          errorType: 'Compile Error'
+        }
+      },
+    }));
+
+    expect(ui.output).to.equal('');
+    expect(ui.errors).to.equal(chalk.red('Compile Error (Babel) in index.js:10:15') + EOL + EOL + chalk.red('Error') + EOL + EOL);
+  });
+
+  it('error title: errorType + nodeName', function() {
+    writeError(ui, new BuildError({
+      broccoliPayload: {
+        broccoliNode: {
+          nodeName: 'Babel'
+        },
+        error: {
+          errorType: 'Compile Error'
+        }
+      },
+    }));
+
+    expect(ui.output).to.equal('');
+    expect(ui.errors).to.equal(chalk.red('Compile Error (Babel)') + EOL + EOL + chalk.red('Error') + EOL + EOL);
+  });
+
+  it('error title: errorType', function() {
+    writeError(ui, new BuildError({
+      broccoliPayload: {
+        error: {
+          errorType: 'Compile Error'
+        }
+      },
+    }));
+
+    expect(ui.output).to.equal('');
+    expect(ui.errors).to.equal(chalk.red('Compile Error') + EOL + EOL + chalk.red('Error') + EOL + EOL);
+  });
+
+  it('error codeFrame', function() {
+    writeError(ui, new BuildError({
+      broccoliPayload: {
+        error: {
+          errorType: 'Compile Error',
+          codeFrame: 'codeFrame'
+        }
+      },
+    }));
+
+    expect(ui.output).to.equal('');
+    expect(ui.errors).to.equal(
+      chalk.red('Compile Error') + EOL + EOL +
+      chalk.red('codeFrame') + EOL + EOL);
+  });
+
+  it('error codeFrame with same error message', function() {
+    writeError(ui, new BuildError({
+      broccoliPayload: {
+        error: {
+          errorType: 'Compile Error',
+          codeFrame: 'codeFrame',
+          message: 'codeFrame'
+        }
+      },
+    }));
+
+    expect(ui.output).to.equal('');
+    expect(ui.errors).to.equal(
+      chalk.red('Compile Error') + EOL + EOL +
+      chalk.red('codeFrame') + EOL + EOL);
+  });
+
+  it('error codeFrame with different error message', function() {
+    writeError(ui, new BuildError({
+      broccoliPayload: {
+        error: {
+          errorType: 'Compile Error',
+          codeFrame: 'codeFrame',
+          message: 'broccoli error message'
+        }
+      },
+    }));
+
+    expect(ui.output).to.equal('');
+    expect(ui.errors).to.equal(
+      chalk.red('Compile Error') + EOL + EOL +
+      chalk.red('broccoli error message') + EOL + EOL +
+      chalk.red('codeFrame') + EOL + EOL);
   });
 });
