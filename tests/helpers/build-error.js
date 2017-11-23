1e731bb286cbda7a1f607bdb4ffbf6a79b359f39
Josemar Luedke <josemarluedke@gmail.com>
2017-11-23 01:07:50 -0800

    Extract useful info from errors on writeError & clean up (#14)

--- tests/helpers/build-error.js
+++ tests/helpers/build-error.js
@@ -10,6 +10,7 @@ function BuildError(input) {
   this.line = input.line;
   this.col = input.col;
   this.stack = input.stack;
+  this.broccoliPayload = input.broccoliPayload;
 }
 
 BuildError.prototype = Object.create(Error.prototype);
